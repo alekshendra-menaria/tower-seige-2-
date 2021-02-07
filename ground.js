1925eff81f6b5d0d82098c4371d6d8248eec80bd
alekshendra-menaria <73381053+alekshendra-menaria@users.noreply.github.com>
2021-02-07 12:58:16 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,18 @@
+class Ground{
+  constructor(){
+      
+var options={
+    isstatic:true
+}
+this.ground=Bodies.rectangle(450,390,900,20,options)
+World.add(world,this.ground);
+  } 
+  display()
+    {
+     var pos= this.ground.position;
+        noStroke();
+        fill(188,67,67);
+        rectMode(CENTER);
+        rect(pos.x,pos.y,900,20);
+    }    
+}
